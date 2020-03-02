c2c4a2d71ec3c2fa96f51d78acaf5524936a6a5d
webhym <webhuangyanmeng@163.com>
2020-03-02 10:43:18 +0800

    vant安装和简单使用

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-      <h2>App根组件</h2>
-      <!-- 路由占位符 -->
-      <router-view></router-view>
-    </div>
+  <div>
+    <h2>App根组件</h2>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <!-- 路由占位符 -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// vant导入
+import Vant from 'vant'
+// vant的样式导入
+import 'vant/lib/index.css'
+// vant的注册
+// 本质：全局方式注册了n多个组件和全局成员
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
