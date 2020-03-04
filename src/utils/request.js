ed4511c79f1885c88a1144bb41328143d9cf4da2
webhym <webhuangyanmeng@163.com>
2020-03-04 19:27:18 +0800

    登录页面布局完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -37,5 +37,48 @@ const instance = axios.create(
   }
 )
 
+// 配置【请求拦截器】
+instance.interceptors.request.use(function (config) {
+  // 判断token存在再做配置(vuex判断)
+  // store.user.token 根据是否有值，就知道用户是否登录系统
+  if (store.state.user.token) {
+    // 注意：token前边有 'Bearer ' 的信息前缀
+    config.headers.Authorization = 'Bearer ' + store.state.user.token
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// 配置【响应拦截器】
+// f1(10,20)
+instance.interceptors.request.use(function (response) {
+  // 正常响应处理
+  // 确认服务器端返回的数据：返回data、返回data.data
+  // const result = await axios()
+  // this.xxx = result
+  try {
+    // data.data如果报错，没有获得到，错误信息会被catch步骤，就走data了
+    return response.data.data
+  } catch (err) {
+    return response.data
+  }
+}, function (error) {
+  // 非正常响应处理(包括401)
+  // console.dir(error) //对象：config request response inAxiosError toJSON
+  if (error.response.status === 401) {
+    // token不ok(token在服务器端已经失效了，2个小时时效)
+    // 强制用户重新登录系统，以刷新服务器端的token时效
+    router.push('./login')
+    // 不要给错误提示了
+    return new Promise(function () {})// 空的Promise对象，没有机会执行catch，进而不做错误提示了
+  }
+  // return new Promise((resolve,reject)=>{
+  // reject('获得文章失败!')
+  // })
+  return Promise.reject(error)
+})
+
 // 对外导出axios对象
 export default instance
--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,13 +1,71 @@
 <template>
-  <div class="page-user-chat">
-    <van-nav-bar left-arrow @click-left="$router.back()" title="登录"></van-nav-bar>
+  <div class="user-login">
+    <!-- 导航栏 -->
+    <van-nav-bar title="登录"></van-nav-bar>
+    <!-- 表单部分
+      vant里边没有form相关组件，只有普通表单域组件
+      van-cell-group是对普通表单域组件做封装
+     -->
+    <van-cell-group>
+      <!--van-field 输入框表单域组件
+        label="手机号" 表单域前边的名字设置
+        required：不进行校验，设置表单域前边有"红星"
+        clearable：表单域内容可以通过右侧“叉号”清除
+      -->
+      <van-field
+        v-model="loginForm.mobile"
+        type="tel"
+        placeholder="请输入手机号码"
+        label="手机号"
+        required
+        clearable
+      />
+      <van-field
+        v-model="loginForm.code"
+        type="password"
+        placeholder="请输入验证码"
+        label="验证码"
+        required
+        clearable
+      >
+        <!-- "命名插槽"应用，提示相关按钮，是要给van-field组件内部的slot去填充的
+        size="small" 设置按钮大小的
+        type="primary" 设置按钮背景颜色
+        -->
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn">
+      <!--van-button
+        type:按钮背景颜色,info是蓝色
+        size:按钮大小的
+        round：圆边效果
+        block：块级样式设置，占据一行
+      -->
+      <van-button type="info" size="small" round block>登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'user-chat'
+  name: 'user-login',
+  data () {
+    return {
+      // 登录表单数据对象
+      // mobile和code是"api数据接口"告诉的，不是自定义的
+      loginForm: {
+        mobile: '', // 手机号码
+        code: '' // 验证码
+      }
+    }
+  }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+// 登录按钮样式
+.login-btn {
+  margin: 40px;
+}
+</style>
